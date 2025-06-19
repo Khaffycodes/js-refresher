83ff0e00b0a324f499280aca8e2d205b5402a9d6
Salau Kafayat <shilekhaffy@gmail.com>
2025-06-19 17:45:51 +0100

    variable & values

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,21 @@
 import { apiKey } from './util.js';
-
-console.log(apiKey);
+//console.log(apiKey);
 
 //aanother way of using import
+// import name from './util.js';
+// console.log(name);
 
-import name from './util.js';
+//aanother way of using import
+//import * as utils from './util.js';
+//console.log(utils.api);
 
-console.log(name);
+//aanother way of using import -as
+// import { test as content, api } from './util.js';
+// console.log(content);
+
+//variables, values and operators
+
+let userMessage = 'Hello Khaffy';
+
+console.log(userMessage);
+console.log(userMessage);
--- assets/scripts/util.js
+++ assets/scripts/util.js
@@ -1,4 +1,6 @@
 export let apiKey = '23456';
+export let api = 'fge457';
+export let test = 'this is for import-as';
 
 //aanother way of using export
 
