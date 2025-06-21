39472368875722cf1765687a93d2e7f3b8f10f31
Salau Kafayat <shilekhaffy@gmail.com>
2025-06-21 15:31:46 +0100

    functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -15,7 +15,34 @@ import { apiKey } from './util.js';
 
 //variables, values and operators
 
-let userMessage = 'Hello Khaffy';
+//let userMessage = 'Hello Khaffy';
 
-console.log(userMessage);
-console.log(userMessage);
+//console.log(userMessage);
+//console.log(userMessage);
+
+//functions
+
+function createGreeting(userName, message = 'Hii') {
+  return message + ' ' + userName;
+}
+
+const greet = createGreeting('Shile');
+console.log(greet);
+
+//exercise
+
+function combine(a, b, c) {
+  return (a * b) / c;
+}
+
+const result = combine(3, 3, 4);
+const result2 = combine(4, 5, 2);
+console.log(result, result2);
+
+//arrow functions
+
+const test = (user, log) => {
+  console.log(user, log);
+};
+
+test('me', '8pm');
