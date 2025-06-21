8cfdd0c3c13ebf2b600e3d9d977eed796e6e2a91
Salau Kafayat <shilekhaffy@gmail.com>
2025-06-21 16:05:56 +0100

    object and arrays

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -31,18 +31,55 @@ console.log(greet);
 
 //exercise
 
-function combine(a, b, c) {
-  return (a * b) / c;
-}
+// function combine(a, b, c) {
+//   return (a * b) / c;
+// }
 
-const result = combine(3, 3, 4);
-const result2 = combine(4, 5, 2);
-console.log(result, result2);
+// const result = combine(3, 3, 4);
+// const result2 = combine(4, 5, 2);
+// console.log(result, result2);
 
 //arrow functions
 
-const test = (user, log) => {
-  console.log(user, log);
-};
+// const test = (user, log) => {
+//   console.log(user, log);
+// };
 
-test('me', '8pm');
+// test('me', '8pm');
+
+//objects nd classes
+
+class User {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greet() {
+    console.log('Hello');
+  }
+}
+
+const user1 = new User('shile', 25);
+console.log(user1);
+user1.greet();
+
+//arrays nd methods
+
+const hobbies = ['swimming', 'hiking', 'cooking'];
+
+hobbies.push('dancing');
+console.log(hobbies);
+
+const index = hobbies.findIndex((item) => {
+  return item === 'cooking';
+});
+
+console.log(index);
+
+const editedHobbies = hobbies.map((item) => item + '!');
+console.log(editedHobbies);
+
+//creating objects with map
+const editedHobbiesObject = hobbies.map((item) => ({ text: item }));
+console.log(editedHobbiesObject);
